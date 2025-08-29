192bac587887a3737c03b91c2d850dd96ba08de4
طالب حوزوي <kay001050@gmail.com>
2025-08-29 17:55:53 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        hawzaGreen: "#1e7f4f",
+        hawzaLight: "#eaf4ef",
+        hawzaGold: "#b89c3c"
+      }
+    }
+  },
+  plugins: []
+}
